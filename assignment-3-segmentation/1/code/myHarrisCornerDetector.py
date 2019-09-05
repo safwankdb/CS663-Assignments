91e08bd148d6b9038ea3ea66c430e40d5fd5f30a
safwankdb <kdbeatbox@gmail.com>
2019-09-06 00:05:02 +0530

    Reduced runtime by factor of 8

--- assignment-3-segmentation/1/code/myHarrisCornerDetector.py
+++ assignment-3-segmentation/1/code/myHarrisCornerDetector.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
 import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
-import cv2
+import time
 
 
 def gaussian_mask(n, sigma=None):
@@ -18,7 +16,19 @@ def gaussian_mask(n, sigma=None):
     return kernel
 
 
-def image_gradient(I):
+def smoothen(I, n=3, sigma=None):
+    kernel = gaussian_mask(n, sigma)
+    h, w = I.shape
+    n = n//2
+    I_s = np.zeros(I.shape)
+    for y in range(n, h-n):
+        for x in range(n, w-n):
+            patch = I[y-n:y+n+1, x-n:x+n+1]
+            I_s[y, x] = np.sum(patch * kernel)
+    return I_s
+
+
+def corner_harris(I, n_g=5, n_w=5, k=0.025):
     h, w = I.shape
     sobel_x = np.array([[-1,  0,  1],
                         [-2,  0,  2],
@@ -28,48 +38,47 @@ def image_gradient(I):
                         [-1, -2, -1]])
     I_x = np.zeros(I.shape)
     I_y = np.zeros(I.shape)
+    D = np.zeros((h, w, 2, 2))
     for y in range(1, h-1):
         for x in range(1, w-1):
             patch = I[y-1:y+2, x-1:x+2]
             I_x[y, x] = np.sum(patch * sobel_x)
             I_y[y, x] = np.sum(patch * sobel_y)
-    return I_x, I_y
-
-
-def solve(I_x, I_y, n, sigma):
-    kernel = gaussian_mask(n, sigma)
+    I_x = smoothen(I_x, n_g)
+    I_y = smoothen(I_y, n_g)
+    for y in range(1, h-1):
+        for x in range(1, w-1):
+            a, b = I_x[y, x], I_y[y, x]
+            D[y, x] = np.array([[a*a, a*b],
+                                [a*b, b*b]])
+    kernel = gaussian_mask(n_w)
+    kernel = np.dstack([kernel]*4)
+    kernel = kernel.reshape(n_w, n_w, 2, 2)
     L_1 = np.zeros(I_x.shape)
     L_2 = np.zeros(I_x.shape)
-    h, w = I_x.shape
     print('Calculating Structure Tensor and Eigenvalues')
+    n = n_w//2
     for y in range(n, h-n):
         for x in range(n, w-n):
-            A = np.zeros((2, 2))
-            for i in range(-n//2, n//2+1):
-                for j in range(-n//2, n//2+1):
-                    W = kernel[i, j]
-                    D = np.array([I_x[y+i, x+j], I_y[y+i, x+j]])
-                    D = D.reshape(2, 1)
-                    D = D.dot(D.T)
-                    A += W * D
+            A = kernel * D[y-n:y+n+1, x-n:x+n+1]
+            A = A.sum((0, 1))
             l1, l2 = np.linalg.eigvals(A)
             L_1[y, x] = l1
             L_2[y, x] = l2
-        print('Progress: {:.02f}{}'.format(100*(y+1-n)/(h-2*n), '%'), end='\r')
-    return L_1, L_2
-
-
-def get_cornerness(L_1, L_2, k):
+        print('Progress: {:.02f}{}'.format(
+            100*(y-2*n)/(h-4*n-2), '%'), end='\r')
+    print('Calculating Corner-ness')
     C = L_1*L_2 - k*(L_1+L_2)**2
-    return C
+    return C, I_x, I_y, L_1, L_2
 
 
-img = Image.open('data/boat.jpg')
-img = np.array(img)
+img_path = 'data/boat.jpg'
+img = np.array(Image.open(img_path).convert('L'))
 img = (img - img.min())/(img.max()-img.min())
-I_x, I_y = image_gradient(img)
-L_1, L_2 = solve(I_x, I_y, 5, 2)
-C = get_cornerness(L_1, L_2, 0.05)
+start = time.time()
+C, I_x, I_y, L_1, L_2 = corner_harris(img)
+stop = time.time()
+print('Time taken: {:.02f}sec'.format(stop - start))
 
 
 plt.figure(figsize=(15, 5))
@@ -91,22 +100,13 @@ plt.imshow(L_2, cmap='gray')
 plt.show()
 
 
-filename = 'data/boat.jpg'
-C = get_cornerness(L_1, L_2, 0.025)
 plt.figure(figsize=(15, 5))
 plt.subplot(121)
-plt.imshow(C)
-plt.title('Our Implementation')
+plt.imshow(img, cmap='gray')
+plt.title('Source Image')
 plt.colorbar()
 plt.subplot(122)
-gray = cv2.imread(filename, 0)
-gray = np.float32(gray)
-dst = cv2.cornerHarris(gray, 5, 3, 0.025)
-plt.imshow(dst)
-plt.title('OpenCV Implementation')
+plt.imshow(C, cmap='gray')
+plt.title('Corner-ness Map')
 plt.colorbar()
-plt.show()
-
-
-plt.imshow(gaussian_mask(5))
 plt.show()
